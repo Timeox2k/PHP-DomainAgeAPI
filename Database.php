bd1bf8be79d113f26c77b63782cb69054bde76fe
Timeox2k <schockilp@gmail.com>
2023-02-19 18:40:14 +0100

    first commit

---
+++ Database.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+if (!class_exists("Database")) {
+    class Database
+    {
+        private static ?Database $instance = null;
+        private PDO $mysql;
+
+        private function __construct()
+        {
+            $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
+            $dotenv->load();
+            $database_host = $_ENV['MYSQL_DB_HOST'];
+            $database_name = $_ENV['MYSQL_DB_NAME'];
+            $database_user = $_ENV['MYSQL_DB_USER'];
+            $database_password = $_ENV['MYSQL_DB_PASSWORD'];
+
+            $mysql = new PDO("mysql:host=$database_host;port=3306;dbname=$database_name", $database_user, $database_password);
+            $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->mysql = $mysql;
+        }
+
+        /**
+         * @return Database
+         */
+        public static function getInstance(): Database
+        {
+            if (self::$instance === null) {
+                self::$instance = new Database();
+            }
+            return self::$instance;
+        }
+
+        /**
+         * @param string $query
+         * @return PDOStatement
+         */
+        public function prepare(string $query): PDOStatement
+        {
+            return $this->mysql->prepare($query);
+        }
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+use Iodev\Whois\Factory;
+
+require './vendor/autoload.php';
+require($_SERVER['DOCUMENT_ROOT'] . "/database.php");
+
+
+if (isset($_GET["domain"])) {
+    $domainName = htmlspecialchars($_GET["domain"]);
+
+    $stmt = Database::getInstance()->prepare("SELECT * FROM domain_data where name = :name LIMIT 1");
+    $stmt->bindParam(":name", $domainName);
+    $stmt->execute();
+    $domainCount = $stmt->rowCount();
+    $databaseDomainData = $stmt->fetch();
+
+    //ALREADY IN DATABASE
+    if ($domainCount == 1) {
+        $databaseEntryTime = strtotime($databaseDomainData["entryTime"]);
+        $time30DaysAgo = strtotime('-4 weeks');
+
+        if ($databaseEntryTime < $time30DaysAgo) {
+            echo $databaseDomainData["age"];
+            exit;
+        }
+    }
+
+    $whois = Factory::get()->createWhois();
+    try {
+        $response = $whois->loadDomainInfo($domainName);
+        $timestamp = 0;
+
+        if ($response->creationDate != null) {
+            $timestamp = $response->creationDate;
+        } else {
+            $timestamp = $response->updatedDate;
+        }
+        echo $timestamp;
+        $entryTime = time();
+        if ($domainCount == 1) {
+            $stmt = Database::getInstance()->prepare("UPDATE domain_data SET age = ?, entryTime = ? WHERE name = ? LIMIT 1");
+            $stmt->execute([$timestamp, $entryTime, $domainName]);
+        } else {
+
+            $stmt = Database::getInstance()->prepare("INSERT INTO domain_data (name,entryTime,age) VALUES (:name, :entry_time, :age);");
+            $stmt->bindParam(":name", $domainName);
+            $stmt->bindParam(":entry_time", $entryTime);
+            $stmt->bindParam(":age", $timestamp);
+            $stmt->execute();
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+
+
+}

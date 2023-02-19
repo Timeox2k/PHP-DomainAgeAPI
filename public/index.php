b93087847dae73798947dcf55aed58799767051c
Timeox2k <schockilp@gmail.com>
2023-02-19 21:58:32 +0100

    Create public folder

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 use TimeoxTwok\DomainAgeApi\DomainAge;
 error_reporting(0);
-require './vendor/autoload.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 if (isset($_GET["domain"])) {
     $domainName = strtolower(htmlspecialchars($_GET["domain"]));
     $domainAge = new DomainAge($domainName);
